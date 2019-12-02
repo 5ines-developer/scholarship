360f3214d45a846674e033777dda488c48db597e
shahirdevp <shahirkm@5ine.in>
2019-12-02 11:48:17 +0530

    copy deleted

--- html-Copy/assets/js/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var instances = M.Sidenav.init(document.querySelectorAll('.sidenav'));
-    var gropDown = M.Dropdown.init(document.querySelectorAll('.dropdown-trigger'), {
-        constrainWidth: false,
-        alignment:'right'
-    })
-    var instances = M.FormSelect.init(document.querySelectorAll('select'));
-});
