19b85821a16edd8f73121e576e6a30a950d00faf
PrathwiEk <49148849+PrathwiEk@users.noreply.github.com>
2019-12-30 13:07:13 +0530

    json

--- application/config/email.php
+++ application/config/email.php
@@ -1,10 +1,9 @@
-<?php 
-
+<?php
 $config['protocol'] = 'smtp';
-$config['smtp_host'] = 'mail.5ine.in';
+$config['smtp_host'] = 'mail.hirewit.com';
 $config['smtp_port'] = 465;
-$config['smtp_user'] = 'test5ine@5ine.in';
-$config['smtp_pass'] = '5ine123#@!';
+$config['smtp_user'] = 'no-reply@hirewit.com';
+$config['smtp_pass'] = 'iAigL_BctZcV';
 $config['mailtype'] = 'html';
 $config['charset'] = 'iso-8859-1';
 $config['wordwrap'] = TRUE;
